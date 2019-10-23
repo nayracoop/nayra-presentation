abae43627560870d410e63d6a3df03bb2af3c536
Ignacio <ignacio.guerra@nayra.coop>
2019-10-23 13:22:43 -0300

    Simple router implementation

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const cover = (props) => {
     return(
-            <div className="cover">
+        <div className="cover">
             <p className="cover-pageNumber">{props.pageNumber}</p>
             <h1 className="cover-title">{props.title}</h1>
         </div>
     );
 }
 
-export default cover
+export default React.memo(cover)
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
 import './App.scss';
 import Cover from '../components/Cover/Cover'
 
@@ -6,9 +7,20 @@ class App extends Component {
   
   render() {
     return (
-      <div className="App">
-        <Cover pageNumber="01" title="¿Quienes Somos?" />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><NavLink to="/about" exact>Quiénes somos</NavLink></li>
+              <li><NavLink to="/art" exact>Artes</NavLink></li>
+            </ul>
+          </nav>
+          <Route path="/" exact>
+            <Cover pageNumber="01" title="¿Quienes Somos?" />
+          </Route>
+        </div>
+      </BrowserRouter>
     );
   }
 }
