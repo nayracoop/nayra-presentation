730321d188ae9234f7558a96e3ef549478aee86c
Ignacio <ignacio.guerra@nayra.coop>
2019-10-24 15:40:29 -0300

    Keyboard navigation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router'
 
 import './App.scss';
 import texts from '../content/content';
@@ -16,43 +17,55 @@ class App extends Component {
   }
 
   state = {
-    navigation: texts.navigation
+    navigation: texts.navigation,
+    currentPage: 0
   }
 
   keyDown = (e) => {
-    if(e.key === 'ArrowRight' || e.key === ' ' || e.key === 'ArrowDown') this.nextScreen();
-    else if(e.key === 'Backspace' || e.key === 'ArrowLeft' || e.key === 'ArrowUp') this.prevScreen();
+    if(e.key === 'ArrowRight' || e.key === ' ' || e.key === 'ArrowDown') {
+      e.preventDefault();
+      this.nextScreen();
+    } else if(e.key === 'Backspace' || e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
+      e.preventDefault();
+      this.prevScreen();
+    }
   }
   
   nextScreen = () => {
-    console.log('next');
+    if(this.state.currentPage < this.state.navigation.length-1) {
+      const currentPage = this.state.currentPage + 1;
+      this.setState({ currentPage: currentPage });
+      this.props.history.push(this.state.navigation[this.state.currentPage].url);
+    }
   }
 
   prevScreen = () => {
-    console.log('prev');
+    if(this.state.currentPage > 0) {
+      const currentPage = this.state.currentPage - 1;
+      this.setState({ currentPage: currentPage });
+      this.props.history.push(this.state.navigation[this.state.currentPage].url);
+    }
   }
   
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Nav options={this.state.navigation} />
-          <Route path="/" exact>
-            <Intro title={texts.intro.title} description={texts.intro.description} date={texts.intro.date} />
-          </Route>
-          <Route path="/about" exact>
-            <Cover pageNumber="01" title="¿Quienes Somos?" />
-          </Route>
-          <Route path="/about/team" exact>
-            <Team pageNumber="01" title="¿Quienes Somos?" />
-          </Route>
-          <Route path="/thanks" exact>
-            <Intro title={texts.end.title} description={texts.end.description} />
-          </Route>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Nav options={this.state.navigation} />
+        <Route path="/" exact>
+          <Intro title={texts.intro.title} description={texts.intro.description} date={texts.intro.date} />
+        </Route>
+        <Route path="/about" exact>
+          <Cover pageNumber="01" title="¿Quienes Somos?" />
+        </Route>
+        <Route path="/about/team" exact>
+          <Team pageNumber="01" title="¿Quienes Somos?" />
+        </Route>
+        <Route path="/thanks" exact>
+          <Intro title={texts.end.title} description={texts.end.description} />
+        </Route>
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
