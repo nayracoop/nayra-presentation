73c991ba72eadd05218f6944b247387af93bb9ea
Ignacio Guerra <ignacio.guerra@nayra.coop>
2019-10-25 03:35:25 -0300

    Creative coding sketch

--- src/components/Art/CreativeCoding.js
+++ src/components/Art/CreativeCoding.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import P5Wrapper from 'react-p5-wrapper';
+import sketch from './sketches/mta';
 import './CreativeCoding.scss';
 
 const CreativeCoding = (props) => {
@@ -9,7 +11,7 @@ const CreativeCoding = (props) => {
                     <h2 className="title">{props.title}</h2>
                     <p className="text">{props.text}</p>
                 </div>
-                <div className="image"><img src={props.image} /></div>
+                <div className="image"><P5Wrapper sketch={sketch}></P5Wrapper></div>
             </div>
         </div>
     );
