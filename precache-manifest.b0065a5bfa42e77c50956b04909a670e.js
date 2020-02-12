f2d998897722ee30249ea1f333150dca0464f70c
Romina Gómez <romina.gomez@nayra.coop>
2020-02-12 15:29:26 -0300

    Updates

---
+++ precache-manifest.b0065a5bfa42e77c50956b04909a670e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2024e84d21a5b240dc81b1ff91e87de",
+    "url": "/nayra-presentation/index.html"
+  },
+  {
+    "revision": "031954f59b34f6bc3dcc",
+    "url": "/nayra-presentation/static/css/main.c036c29f.chunk.css"
+  },
+  {
+    "revision": "b9fb496f1d9de9c71c02",
+    "url": "/nayra-presentation/static/js/2.4dbbfcd4.chunk.js"
+  },
+  {
+    "revision": "031954f59b34f6bc3dcc",
+    "url": "/nayra-presentation/static/js/main.c84dbf73.chunk.js"
+  },
+  {
+    "revision": "cf97471380daf8513af9",
+    "url": "/nayra-presentation/static/js/runtime-main.e3207181.js"
+  },
+  {
+    "revision": "17b5c661a720b604e9be329f3396742c",
+    "url": "/nayra-presentation/static/media/_facttic.17b5c661.jpg"
+  },
+  {
+    "revision": "d4971d6d1a1dab051ef1a60ad87312b3",
+    "url": "/nayra-presentation/static/media/artes.d4971d6d.jpg"
+  },
+  {
+    "revision": "0862e5d8f305692f464dc98014d3e976",
+    "url": "/nayra-presentation/static/media/facttic.0862e5d8.jpg"
+  },
+  {
+    "revision": "d508b463e2f7887b5d1b8cdcb9749330",
+    "url": "/nayra-presentation/static/media/fit.d508b463.png"
+  },
+  {
+    "revision": "b19a0aebe93eb2769a7e4cf8bcc404ae",
+    "url": "/nayra-presentation/static/media/icon-1.b19a0aeb.svg"
+  },
+  {
+    "revision": "c780571c27e7a092ba910e06aa875ef7",
+    "url": "/nayra-presentation/static/media/icon-2.c780571c.svg"
+  },
+  {
+    "revision": "6f4960dafc9f20bbe6adbbde1a3d8343",
+    "url": "/nayra-presentation/static/media/lineas.6f4960da.svg"
+  },
+  {
+    "revision": "d50399309388dd465416f0ffb9ada9f5",
+    "url": "/nayra-presentation/static/media/logo-nayra.d5039930.svg"
+  },
+  {
+    "revision": "c24e0e392a815deb6908be7ed92646bc",
+    "url": "/nayra-presentation/static/media/nacho.c24e0e39.svg"
+  },
+  {
+    "revision": "8c53956d6dd386edcb592898c47ff7a5",
+    "url": "/nayra-presentation/static/media/nayres.8c53956d.jpg"
+  },
+  {
+    "revision": "561c8f0ac909e90ca5bc88c43f50e952",
+    "url": "/nayra-presentation/static/media/romi.561c8f0a.svg"
+  },
+  {
+    "revision": "fef152204ec28637d5a2462d5d15ee77",
+    "url": "/nayra-presentation/static/media/tobi.fef15220.svg"
+  }
+]);
